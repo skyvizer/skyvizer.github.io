fc5ee9e61b5d318ed040f141a58546b0264ccdad
skyvizer <skyvizer@gmail.com>
2018-06-18 11:41:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# kagerou
+
+ACT-OverlayPlugin skin for FF14. pronounced as /ka-ge-row/ (not j-like g).
+
+Modern, Material Design, easily configurable, cloud-hosted and always up-to-date.
+
+## Warning
+
+### **OverlayPlugin 0.3.3.11** Required!!!
+
+If you are using older version, settings WILL NOT saved!
+
+If there's 2 buttons: you are on legacy. Get [Latest](https://github.com/hibiyasleep/OverlayPlugin/releases/latest) here.
+   There should be 3 buttons, named like this: `Copy`, `Open DevTools`, `Reload`. See screenshots below.
+
+---
+
+## Usage
+
+### Quick setup
+
+No download needed, please first read `Warning` above.
+Use this as overlay URL:
+
+> `https://hibiyasleep.github.io/kagerou/overlay`
+
+![act-settings](https://veltall.github.io/kagerou/images/act-settings.png)
+
+### Detailed
+
+1. Open ACT.
+2. Plugins > OverlayPlugin.dll > Add 'Mini Parse' with any name (e.g. Totoro).
+3. Read warning below.
+4. Go to the new tab (e.g. Totoro), and set url as above.
+
+![detailed instructions](https://veltall.github.io/kagerou/images/totoro.png)
+
+
+## Features / Screenshots
+
+![Overlay Preview](https://d.hibiya.moe/obZ.png)
+
+* RDPS/RHPS
+* Solo Mode
+* History lookup
+* Merge/Unmerge Pet stats
+* Blur/Unblur other user's name
+* Configurable info table (column width, ordering, colors, etc.)
+* (WIP) Display abbreviated name, instead of full long name
+* Localization: Korean, English, or Add your language!
+
+---
+
+![Settings window - General](https://d.hibiya.moe/zLm.png)
+
+* Fully configurable, no Notepad! (Settings require `OverlayPlugin 0.3.3.11` to **Save**)
+
+---
+
+![CSS Customizing; HotS logo spinning](https://d.hibiya.moe/rne.png)
+
+* Custom CSS: Setting isn't enough? Expand as you want!  
+Use the DevTools to find the CSS selectors that you wish to modify.
+
+![how to use custom CSS and DevTools](https://veltall.github.io/kagerou/images/custom-css.png)
+
+---
+
+## Contribute
+
+### If you are not developer
+
+Since I only play on korean server, I need your feedback and suggestion for
+Global server support.
+
+Pending features:
+
+* ?
+
+### Else
+
+* Pull Requests is welcome, but please keep this code clean as possible.
+* 2 spaces.
+* NO semicolon in Javascript, except `;(function`.
+* 80 char per line. Inline SVG is an exception, keep them in one line.
+* No raster images; exclude job icons, use SVG.
+* Target is Chrome 45, when support of other platform is needed and code should
+  follow that; then I'll use Babel.
+* Locale file is in `share/lang`.
+
+## License
+
+GPLv3.
---
+++ config/lib/ui.js
@@ -0,0 +1,206 @@
+'use strict'
+
+const switchTab = function switchTab(target) {
+  $('section.active', 0).classList.remove('active')
+  $(`section[data-page='${target}']`, 0).classList.add('active')
+  let targetLi = $(`li[data-page='${target}']`, 0)
+
+  $('header h2', 0).textContent = $(`li[data-page='${target}']`, 0).textContent
+  $('header h2', 0).setAttribute(
+    'data-locale',
+    $(`li[data-page='${target}'] > label`, 0).getAttribute('data-locale')
+  )
+
+  $('body', 0).classList.toggle('hide-header', target === 'info')
+}
+
+const sendMessage = function sendMessage (message) {
+  if(window.OverlayPluginApi) {
+    OverlayPluginApi.broadcastMessage(message)
+  } else if(window.opener) {
+    window.opener.postMessage(message, '*')
+  } else {
+    new dialog('error', {
+      title: window.locale.get('ui.config.dialog.manual-reload.title'),
+      content: window.locale.get('ui.config.dialog.manual-reload.content')
+    })
+  }
+}
+
+;(function() {
+
+
+  window.addEventListener('load', function(e) {
+
+    // menu switcher
+    $map('.menu li[data-page]', _ => {
+      let target = _.getAttribute('data-page')
+      _.addEventListener('click', function() {
+        switchTab(target)
+      })
+    })
+
+    // tab switcher
+
+    $map('.tab-switcher', element => {
+      let target = element.getAttribute('data-target')
+      let targets = $(target, 0)
+
+      ;[].forEach.call(element.getElementsByTagName('li'), _ => {
+        _.addEventListener('click', function() {
+          let id = this.getAttribute('data-pane')
+          $(element, '.active', 0).classList.remove('active')
+          $(targets, '.active', 0).classList.remove('active')
+          $(element, `li[data-pane='${id}']`, 0).classList.add('active')
+          $(targets, `[data-pane='${id}']`, 0).classList.add('active')
+        })
+      })
+    })
+
+    // colwidth
+    ;(function(){
+      const columns = COLUMN_INDEX
+      const _container = $('section[data-page=width]', 0)
+
+      for(let k1 in columns) {
+        let article = '<article>'
+
+        article += `<h3 data-locale="col.${k1}._"> </h3>`
+
+        for(let k2 in columns[k1]) {
+          if(k2 === '_' || k2 === 'icon') continue
+          let id = `input-colwidth-_${k1}-${k2}`
+
+          article += `
+<p class="control">
+  <label for="${id}" data-locale="col.${k1}.${k2}.1"> </label>
+  <span class="input-group">
+    <label class="input-value" for="${id}"> ... </label>
+    <input type="range" id="${id}" min="1.5" step="0.5" max="9"
+           data-config-key="colwidth._${k1}-${k2}" />
+  </span>
+</p>`
+        }
+        article += '</article>'
+
+        _container.insertAdjacentHTML('beforeend', article)
+      }
+    })()
+
+    // load config & fill all inputs
+    $map('[data-config-key]', _ => {
+      let placeholder = _.getAttribute('placeholder')
+      let value = config.get(_.getAttribute('data-config-key'))
+      let type = _.getAttribute('data-type')
+      let unit = _.getAttribute('data-unit') || ''
+
+      if(type === 'array') {
+        _.value = value.join(', ')
+      } else if(type === 'boolean') {
+        _.checked = value
+      } else if(type === 'integer') {
+        _.value = +value
+      } else {
+        _.value = ((value || '') + '').replace(new RegExp(unit, 'g'), '')
+      }
+
+      // placeholder glitch workaround
+      _.setAttribute('placeholder', '')
+      _.setAttribute('placeholder', placeholder || '')
+    })
+
+    // listen on input changes, and show the value
+    $map('.input-value', _ => {
+      let target = $('#' + _.getAttribute('for'))
+      _.textContent = config.get(target.getAttribute('data-config-key'))
+      let unit = target.getAttribute('data-unit') || ''
+
+      target.addEventListener('input', function(e) {
+        _.textContent = this.value + unit
+      })
+    })
+
+    $map('.input-value-style', _ => {
+      let target = $('#' + _.getAttribute('for'))
+      let key = target.getAttribute('data-style')
+      let unit = target.getAttribute('data-unit') || ''
+
+      _.style[key] = config.get(target.getAttribute('data-config-key'))
+
+      target.addEventListener('input', function(e) {
+        _.style[key] = this.value + unit
+      })
+    })
+
+    // re-zero
+    $map('[data-reset]', _ => {
+      _.addEventListener('click', function(e) {
+        let key = this.getAttribute('data-reset') || false
+        new dialog('confirm', {
+          title: window.locale.get('ui.config.dialog.really'),
+          content: window.locale.get('ui.config.dialog.undone'),
+          callback: _ => {
+            config.reset(key)
+            sendMessage('reload')
+            location.reload()
+          }
+        })
+      })
+    })
+
+    // save
+    $('#save').addEventListener('click', _ => {
+      [].map.call($('[data-config-key]'), o => {
+        let value = o.value
+        let type = o.getAttribute('data-type')
+        let unit = o.getAttribute('data-unit') || ''
+        let key = o.getAttribute('data-config-key')
+
+        if(type === 'array') {
+          value = value.split(o.getAttribute('data-splitter')).map(_ => _.trim())
+        } else if(type === 'boolean') {
+          value = o.checked
+        } else {
+          value += unit
+        }
+
+        config.set(key, value)
+      })
+
+      window.locale.setLang(config.get('lang'))
+
+      tabconfig.save()
+
+      // css editor
+
+      config.set('custom_css', window.editor.getValue())
+
+      config.save()
+      sendMessage('restyle')
+    })
+
+    if(navigator.userAgent.indexOf('QtWebEngine') !== -1) {
+      $('.ws-tab-notice', 0).classList.remove('hidden')
+      let dragging
+
+      const copyEvent = (type, event) => {
+        let clone = new event.constructor(event.type, event)
+        clone.cloned = true
+        return clone
+      }
+
+      document.addEventListener('mousedown', _ => {
+        console.log('mousedown, dragging:', dragging, ', event:', _)
+        if(dragging) {
+          dragging = false
+          e = copyEvent('dragend', _)
+          console.log('trying to send drop event to sortable with:', e)
+          window.tabconfig.sortable._onDrop(e)
+        }
+      })
+      document.addEventListener('dragstart', _ => { dragging = true; console.log('set dragging true:', dragging) })
+    }
+
+  })
+
+})()
